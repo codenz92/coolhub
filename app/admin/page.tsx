8b14cf2aa699d67193ddc0428c8daef54b0ca376
codenz92 <78385742+codenz92@users.noreply.github.com>
2026-02-02 06:56:47 +0000

    added for dev

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -8,7 +8,7 @@ export default async function AdminPage() {
     const session = await auth(); //
 
     // Replace 'admin_user' with your actual username for security
-    if (session?.user?.username !== "admin_user") {
+    if (session?.user?.username !== "dev") {
         redirect("/dashboard");
     }
 
