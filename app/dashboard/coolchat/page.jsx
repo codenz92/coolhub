7276a5a21064e5b4c35313e1926181bec7380352
codenz92 <78385742+codenz92@users.noreply.github.com>
2026-02-03 05:43:26 +0000

    ui update

--- app/dashboard/coolchat/page.jsx
+++ app/dashboard/coolchat/page.jsx
@@ -161,7 +161,7 @@ export default function CoolChat() {
             <Link
               href="/dashboard"
             >
-              ← RETURN TO DASHBOARD
+              🔙 RETURN TO DASHBOARD 🔙
             </Link>
           </div>
         </div>
